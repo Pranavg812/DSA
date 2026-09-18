87697b78cd18592a8405fd433db07c07949ad208
Pranav Goyal <pranavgoyal0812@gmail.com>
2026-09-18 10:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.32%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = pow(x,n);
+        //double ans = pow(x,n);
+        //return ans;
+        double ans = 1;
+        long bF= n;
+        if (bF<0){
+            x = 1/x;
+            bF = -bF;
+        }
+
+        while (bF>0){
+            if (bF % 2 ==1){
+                ans = ans *x;
+            }
+            x = x*x;
+            bF = bF/2;
+        }
         return ans;
     }
 };
